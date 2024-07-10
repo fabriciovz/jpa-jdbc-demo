3e32e5ca4c0021610b46a65d42e646da390343ce
Fabricio Bravo Guevara <fabribraguev@gmail.com>
2024-07-10 06:29:38 -0400

    creating jpa/jdbc onboarding repo

---
+++ src/main/java/com/fabribraguev/springboot/jpa_jdbc_demo/JpaJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fabribraguev.springboot.jpa_jdbc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJdbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaJdbcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabribraguev/springboot/jpa_jdbc_demo/JpaJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabribraguev.springboot.jpa_jdbc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
